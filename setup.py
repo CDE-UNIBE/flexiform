e687d7cebfeb16ab686d742e0ac1deaf33b5adc1
Marc Zimmermann <marc.zimmermann@89grad.ch>
2017-11-07 11:18:24 +0100

    Add minimal setup.py
    This allows local installation with pip install -e <project_path>

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='flexiform',
+      version='0.1.0',
+      description='Questions to Visualization',
+      url='https://github.com/CDE-UNIBE/changeme',
+      author='Marc Zimmermann',
+      author_email='marc.zimmermann@89grad.ch',
+      license='',
+      packages=['flexiform'],
+      zip_safe=False)
