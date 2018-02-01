1033855a94785cf8fcd3fade79af0ef2ac77b561
Sebastian Manger <s.manger@gmx.ch>
2018-02-01 12:15:51 +0100

    Include (sub)folders when creating the package

--- setup.py
+++ setup.py
@@ -3,9 +3,10 @@ from setuptools import setup
 setup(name='flexiform',
       version='0.1.0',
       description='Questions to Visualization',
-      url='https://github.com/CDE-UNIBE/changeme',
-      author='Marc Zimmermann',
-      author_email='marc.zimmermann@89grad.ch',
+      url='https://github.com/CDE-UNIBE/flexiform',
+      author='CDE Webdev',
+      author_email='wocat-webdev@cde.unibe.ch',
       license='',
+      include_package_data=True,
       packages=['flexiform'],
       zip_safe=False)
